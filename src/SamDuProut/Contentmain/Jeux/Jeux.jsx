1a198d75668d531a4a15f7ea26b055350cd17ba9
alerwann <alerwann411@gmail.com>
2025-03-12 11:01:51 +0100

    fin réseaux début jeu

--- src/SamDuProut/Contentmain/Jeux/Jeux.jsx
+++ src/SamDuProut/Contentmain/Jeux/Jeux.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { listeJeux } from "./ListeJeux"
 
 
 
 const Jeux =()=>{
+    const [searchTerm, setSearchTerm] = useState('');
+    const filteredData = listeJeux.filter((item) =>
+        item.nom.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+ 
     return(
+        
         <div>
-            {listeJeux.map((jeu,index)=>(
+            <div>
+                <label id="Choice">Les jeux auxquel il rejoue</label>
+                <input type="checkbox" name="choice" id="Choice"/>
+                <label i="gameName">Quel jeu cherches-tu?</label>
+                <input type="text" name="gameName" id="gameName"  onchange={(e)=>setSearchTerm(e.target.value)}/>
+            </div>
+
+
+            {filteredData.map((jeu,index)=>(
              
                         <div className="jeux" key={index}>
                             <p>Nom: {jeu.nom}</p>
